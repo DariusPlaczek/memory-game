ab3913b1a02fae68e91e066b5b1d95ab32352828
Darius Placzek <dariusplaczek@gmail.com>
2021-02-01 20:10:08 +0100

    for a community

--- src/Data/playlist.js
+++ src/Data/playlist.js
@@ -1,14 +1,10 @@
 const playlist = [
   {
-    src: "https://jsons.darius-design.de/memory/Sound/Ambient_Music_by_Michael-K_long.mp3",
-    title: "Michael-K",
-    artist: "Ambient_Music",
+    src: "YOUR URL",
+    title: "TITLE",
+    artist: "ARTIST",
   },
-  {
-    src: "https://jsons.darius-design.de/memory/Sound/Inspiring-Ambient-Background.mp3",
-    title: "Inspiring-Ambient-Background",
-    artist: "StockMusicPro",
-  },
+
 ];
 
 export { playlist };
